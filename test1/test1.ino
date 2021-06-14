1e7b4ffc9d74f0c5d3056dac4c4201d8997d3673
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-06-14 17:29:07 +0200

    copy code moteurs

--- test1/test1.ino
+++ test1/test1.ino
@@ -1,22 +1,5 @@
 #include <Servo.h>
-
 #include <Arduino.h>
-
-/* Senseur de distance Ultrason HC-SR04:
- VCC sur Arduino 5v
- GND sur Arduino GND
- Echo sur Arduino broche 7
- Trig sur Arduino broche 8
-
- Ce sketch/croquis est originaire de Virtualmix: http://goo.gl/kJ8Gl
- Il a été modifié par Winkle ink ici:
-   http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
- Puis modifié par ScottC le 10 Nov 2012:
-   http://arduinobasics.blogspot.com/
- Finalement traduit par MCHobby.be pour le tutoriel:
-   http://wiki.mchobby.be/index.php?title=HC-SR04
- */
-
 
 #define echoPin 7 // broche Echo
 #define trigPin 8 // broche Trigger (declenchement)
@@ -27,6 +10,15 @@ int minimumRange = 10;   // distance Minimale acceptée (en cm)
 long duration, distance; // Durée utilisé pour calculer la distance
 Servo monServo;
 
+// Motor A connections
+int enA = 9;
+int in1 = 5;
+int in2 = 4;
+// Motor B connections
+int enB = 10;
+int in3 = 3;
+int in4 = 2;
+
 void setup() {
     // Activer la communication série
     Serial.begin (9600);
@@ -35,13 +27,74 @@ void setup() {
     pinMode(echoPin, INPUT);
     pinMode(LEDPin, OUTPUT); // activer la LED sur la carte (si nécessaire)
     monServo.attach(6);
+    // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
-// Partie du code continuellement exécuté
-// Son but est d'effectuer un cycle de détection pour déterminer
-// la distance de l'objet le plus proche (par réverbération de
-// l'onde sur ce dernier)
-//
+void speedControl() {
+  // Turn on motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  
+  // Accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+  
+  // Decelerate from maximum speed to zero
+  for (int i = 255; i >= 0; --i) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
+  
+  // Now turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+void directionControl() {
+  // Set motors to maximum speed
+  // For PWM maximum possible values are 0 to 255
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+
+  // Turn on motor A & B
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(2000);
+  
+  // Now change motor directions
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  delay(2000);
+  
+  // Turn off motors
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
 void looker(){
    
     // Envoi une impulsion de 10 micro seconde sur la broche "trigger"
@@ -84,7 +137,6 @@ void looker(){
           for (int position = 0; position <=180; position ++){ // on crée une variable position qui prend des valeurs entre 0 à 180 degrés
         monServo.write(position);  // le bras du servomoteur prend la position de la variable position
         delay(0);  // on attend 15 millisecondes
-                Serial.println("toto");
     }
 
     }
@@ -107,7 +159,11 @@ void enginer(){
     }
 }
 void loop() {
-    // put your main code here, to run repeatedly:
-   looker();
+   // put your main code here, to run repeatedly:
+  // looker();
   // enginer();
+  //directionControl();
+  delay(1000);
+  //speedControl();
+  delay(1000);
 }
